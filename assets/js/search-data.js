7644bfa8b76a00215af3a8ffa76ae9e730407d13
pranavbhosale25 <pranavbhosale25@gmail.com>
2026-06-30 03:33:01 +0000

    Deploying to gh-pages from @ pranavbhosale25/pranavbhosale25.github.io@ef92e1aa7044327970034c955363e664a92244a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,14 +436,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%70%72%61%6E%61%76@%63%6D%75.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/pranavbhosale2502", "_blank");
         },
       },{
         id: 'social-rss',
@@ -458,13 +458,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
